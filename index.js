dffa69500a425815be7e297526cb426061a7e7c5
roshan <roshnn74@gmail.com>
2022-08-05 16:27:53 +0530

    make it better

--- index.js
+++ index.js
@@ -17,14 +17,18 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-app.get('/', (res,req) => {
-    res.send('App running');
-});
-
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://roshnn74:Reaperoflife@cluster0.txgtj.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser:true, useUnifiedTopology:true })
             .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
             .catch((error) => console.log(error.message));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname,'client','build,index.html'));
+    });
+}
 
 // mongoose.set('useFindAndModify', false); //was part of tutorial but error
