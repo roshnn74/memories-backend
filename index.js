88d3d0a861f633aa2233d3159451262ca853da07
roshan <roshnn74@gmail.com>
2022-08-05 17:21:44 +0530

    my first commit

--- index.js
+++ index.js
@@ -19,16 +19,15 @@ app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://roshnn74:Reaperoflife@cluster0.txgtj.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser:true, useUnifiedTopology:true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser:true, useUnifiedTopology:true })
             .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
             .catch((error) => console.log(error.message));
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-    app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname,'client','build,index.html'));
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
 
 // mongoose.set('useFindAndModify', false); //was part of tutorial but error
