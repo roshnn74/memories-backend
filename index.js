ab3a394dfe7e1da8d12fc2cc6366d27c78a8fb85
roshan <roshnn74@gmail.com>
2022-08-12 10:56:59 +0530

    make it better

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/', (req,res) => {
 const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = 'mongodb+srv://roshnn74:Reaperoflife@cluster0.txgtj.mongodb.net/?retryWrites=true&w=majority';
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
+
 mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true })
             .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
             .catch((error) => console.log(error.message));
