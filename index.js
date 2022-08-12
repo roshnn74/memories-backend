217bf1fca66fa06530a7a2bb27fe3d4ae2723f71
roshan <roshnn74@gmail.com>
2022-08-12 09:01:00 +0530

    make it better

--- index.js
+++ index.js
@@ -23,11 +23,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser:true, useUnifiedTopolo
             .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
             .catch((error) => console.log(error.message));
 
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
-
 
 // mongoose.set('useFindAndModify', false); //was part of tutorial but error
